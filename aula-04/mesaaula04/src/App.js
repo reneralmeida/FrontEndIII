7135d41e201efeff5c33f8a6829b306d564025a8
Rener <rener.almeida@gmail.com>
2021-10-25 13:12:17 -0300

    dollar-bill-yo

--- aula-04/mesaaula04/src/App.js
+++ aula-04/mesaaula04/src/App.js
@@ -7,9 +7,12 @@ function App() {
       <header className="App-header">
         <img src={test} className="App-logo" alt="logo" />
         <p>
-          Homem da grana
+          Dollar dollar bill yo
         </p>
       </header>
+      <div>
+      <iframe width="720" height="576" src="https://www.youtube.com/embed/sXJXLq1lN7U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
     </div>
   );
 }
